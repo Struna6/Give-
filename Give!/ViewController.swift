e7a1034c06c7502444f7d8dce4321dbde0af7bf5
Struna6 <struniawski.karol@gmail.com>
2019-09-14 21:00:13 +0200

    Initial Commit

---
+++ Give!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Give!
+//
+//  Created by Karol Struniawski on 14/09/2019.
+//  Copyright © 2019 Karol Struniawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
